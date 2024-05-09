fe9921821d8897597739e6330e5c1071de8d3104
suico <ryoorer@gmail.com>
2024-05-09 13:24:08 +0900

    update: kv

--- src/app/_sections/kv.js
+++ src/app/_sections/kv.js
@@ -2,32 +2,64 @@ import Image from "next/image";
 
 export default function KV() {
   return (
-    <div>
-      <p className="text-secondary bg-primary">kv</p>
-      <h1 className="text-9xl font-bold tracking-tighter leading-none">
-        FLYING CAT LLC
-      </h1>
-      <Image
-        src="/anim-monster/1.png"
-        alt="monster"
-        width={1080}
-        height={1080}
-        priority
-      />
-      <Image
-        src="/anim-monster/2.png"
-        alt="monster"
-        width={1080}
-        height={1080}
-        priority
-      />
-      <Image
-        src="/anim-monster/3.png"
-        alt="monster"
-        width={1080}
-        height={1080}
-        priority
-      />
+    <div className="w-full" style={{ height: '80vh' }}>
+      <div className="w-full h-full flex flex-col justify-center">
+        <h1>
+          <div className="mb-8">
+            <Image
+              src="/company_name.png"
+              alt="FLYING CAT LLC"
+              style={{ width: '45%' }}
+              width="1766"
+              height="1760"
+            />
+          </div>
+        </h1>
+        <p className="font-bold">
+          空とぶネコは地域の生産者や製造者が挑む
+          <br/>
+          事業開発や企業再生をハンズオンで支援する
+          <br/>
+          宮古列島最大規模のブティックファームです。
+        </p>
+        <div class="flex -mt-12">
+          <Image
+            src="/anim-monster/6.png"
+            alt="monster"
+            style={{ width: "18%", height: "auto", objectFit: "contain"}}
+            width="1766"
+            height="1760"
+          />
+          <Image
+            src="/anim-monster/7.png"
+            alt="monster"
+            style={{ width: "18%", height: "auto", objectFit: "contain" }}
+            width="1766"
+            height="1760"
+          />
+          <Image
+            src="/cat/default.png"
+            alt="monster"
+            style={{ width: "28%", height: "auto", objectFit: "contain" }}
+            width="1766"
+            height="1760"
+          />
+          <Image
+            src="/anim-monster/3.png"
+            alt="monster"
+            style={{ width: "18%", height: "auto", objectFit: "contain" }}
+            width="1766"
+            height="1760"
+          />
+          <Image
+            src="/anim-monster/2.png"
+            alt="monster"
+            style={{ width: "18%", height: "auto", objectFit: "contain" }}
+            width="1766"
+            height="1760"
+          />
+        </div>
+      </div>
     </div>
   );
 }
