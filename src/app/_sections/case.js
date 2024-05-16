9ef3aad7288b152c847c233c379da1456ea2fc38
Toshihiro Kojima <toshihiro.l.kojima@gmail.com>
2024-05-16 16:59:07 +0900

    refactor: case sp

--- src/app/_sections/case.js
+++ src/app/_sections/case.js
@@ -7,12 +7,16 @@ import {monsters} from '../_constants/monster';
 export default function Case() {
   gsap.registerPlugin(ScrollTrigger);
 
-  const item1Ref = useRef(null);
-  const item1NumberRef = useRef(null);
-  const item1MonsterRef = useRef(null);
-  const item2Ref = useRef(null);
-  const item2NumberRef = useRef(null);
-  const item2MonsterRef = useRef(null);
+  const monsterItemRefs = useRef([]);
+  const monsterNumberRefs = useRef([]);
+  const monsterMonsterRefs = useRef([]);
+
+  monsters.forEach((_, index) => {
+    monsterItemRefs.current[index] = createRef();
+    monsterNumberRefs.current[index] = createRef();
+    monsterMonsterRefs.current[index] = createRef();
+  });
+
   const itemLastRef = useRef(null);
   const itemLastMonsterRef = useRef(null);
 
@@ -22,48 +26,26 @@ export default function Case() {
     if (didEffect.current) return;
     didEffect.current = true;
 
-    const item1Element = item1Ref.current;
-    const item1NumberElement = item1NumberRef.current;
-    const item1MonsterElement = item1MonsterRef.current;
-    const item2Element = item2Ref.current;
-    const item2NumberElement = item2NumberRef.current;
-    const item2MonsterElement = item2MonsterRef.current;
     const itemLastElement = itemLastRef.current;
     const itemLastMonsterElement = itemLastMonsterRef.current;
 
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: item1Element,
-          start: 'center center',
-          pin: true,
-          scrub: true,
-          // markers: true,
-        },
-      })
-      .to(item1NumberElement, {
-        rotate: 360,
-      })
-      .from(item1MonsterElement, {
-        opacity: 0,
-      });
-
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: item2Element,
-          start: 'center center',
-          pin: true,
-          scrub: true,
-          // markers: true,
-        },
-      })
-      .to(item2NumberElement, {
-        rotate: 360,
-      })
-      .from(item2MonsterElement, {
-        opacity: 0,
-      });
+    monsters.forEach((_, index) => {
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: monsterItemRefs.current[index].current,
+            start: 'center center',
+            pin: true,
+            scrub: true,
+          },
+        })
+        .to(monsterNumberRefs.current[index].current, {
+          rotate: 360,
+        })
+        .from(monsterMonsterRefs.current[index].current, {
+          opacity: 0,
+        });
+    });
 
     gsap
       .timeline({
@@ -72,7 +54,6 @@ export default function Case() {
           start: 'center center',
           pin: true,
           scrub: true,
-          // markers: true,
         },
       })
       .from(itemLastMonsterElement, {
@@ -83,101 +64,56 @@ export default function Case() {
 
   return (
     <div className="bg-blue-900">
-      <div
-        ref={item1Ref}
-        className="h-screen w-full p-6 flex flex-col justify-center items-center"
-      >
-        <div className="h-full max-h-700 max-w-sm flex flex-col bg-white p-6">
-          <div className="w-full bg-yellow-100 aspect-square relative mb-4">
-            <Image
-              ref={item1NumberRef}
-              src="/zukan/number/1.png"
-              alt="01"
-              className="absolute top-6 right-6"
-              width={36}
-              height={40}
-              priority
-            />
+      {monsters.map((m, i) => (
+        <div
+          ref={monsterItemRefs.current[i]}
+          key={m.name.en}
+          className="h-screen w-full p-6 flex flex-col justify-center items-center"
+        >
+          <div className="h-full max-h-700 max-w-sm flex flex-col bg-white p-6">
+            <div className="w-full bg-yellow-100 aspect-square relative mb-4">
+              <Image
+                ref={monsterNumberRefs.current[i]}
+                src={`/zukan/number/${i + 1}.png`}
+                alt="01"
+                className="absolute top-6 right-6"
+                width={36}
+                height={40}
+                priority
+              />
 
-            <Image
-              ref={item1MonsterRef}
-              src="/anim-monster/1.png"
-              alt="monster"
-              width={1080}
-              height={1080}
-              priority
-            />
-          </div>
+              <Image
+                ref={monsterMonsterRefs.current[i]}
+                src={`/anim-monster/${i + 1}.png`}
+                alt="monster"
+                width={1080}
+                height={1080}
+                priority
+              />
+            </div>
 
-          <div className="h-full flex flex-col justify-between">
-            <div>
-              <p className="text-xs mb-2">妖怪</p>
-              <p className="text-3xl font-bold">紙かくし</p>
-              <p className="text-sm">KAMIKAKUSHI</p>
-            </div>
-            <div>
-              <p>生態</p>
-              <p>人間の焦る顔が大好き。</p>
-              <p>人間が大事にしている紙を根こそぎ盗んでしまう。</p>
-              <p>紙の中でも特に紙幣を好み、</p>
-              <p>盗んだ紙は寝床として敷き詰めている。</p>
-            </div>
-            <div>
-              <p>事象</p>
-              <p>売上が少ない、利益率が低い、</p>
-              <p>キャッシュフローが悪い。</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div
-        ref={item2Ref}
-        className="h-screen w-full p-6 flex flex-col justify-center items-center"
-      >
-        <div className="h-full max-h-700  max-w-sm flex flex-col bg-white p-6">
-          <div className="w-full bg-pink-100 aspect-square relative mb-4">
-            <Image
-              ref={item2NumberRef}
-              src="/zukan/number/2.png"
-              alt="01"
-              className="absolute top-6 right-6"
-              width={36}
-              height={40}
-              priority
-            />
-
-            <Image
-              ref={item2MonsterRef}
-              src="/anim-monster/2.png"
-              alt="monster"
-              width={1080}
-              height={1080}
-              priority
-            />
-          </div>
-
-          <div className="h-full flex flex-col justify-between">
-            <div>
-              <p className="text-xs mb-2">妖怪</p>
-              <p className="text-3xl font-bold">つまらん象</p>
-              <p className="text-sm">TSUMARAN ZOU</p>
-            </div>
-            <div>
-              <p>生態</p>
-              <p>人間の焦る顔が大好き。</p>
-              <p>人間が大事にしている紙を根こそぎ盗んでしまう。</p>
-              <p>紙の中でも特に紙幣を好み、</p>
-              <p>盗んだ紙は寝床として敷き詰めている。</p>
-            </div>
-            <div>
-              <p>事象</p>
-              <p>売上が少ない、利益率が低い、</p>
-              <p>キャッシュフローが悪い。</p>
+            <div className="h-full flex flex-col justify-between">
+              <div>
+                <p className="text-xs mb-2">妖怪</p>
+                <p className="text-3xl font-bold">{m.name.ja}</p>
+                <p className="text-sm">{m.name.en}</p>
+              </div>
+              <div>
+                <p>生態</p>
+                <p>人間の焦る顔が大好き。</p>
+                <p>人間が大事にしている紙を根こそぎ盗んでしまう。</p>
+                <p>紙の中でも特に紙幣を好み、</p>
+                <p>盗んだ紙は寝床として敷き詰めている。</p>
+              </div>
+              <div>
+                <p>事象</p>
+                <p>売上が少ない、利益率が低い、</p>
+                <p>キャッシュフローが悪い。</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
 
       <div
         ref={itemLastRef}
