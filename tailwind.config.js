dc50ed6f0c7c831cf5572ba7bb82ddb14a63b788
Toshihiro Kojima <toshihiro.l.kojima@gmail.com>
2024-05-18 08:27:38 +0900

    feat: font mundial & mochiy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mundial: ['mundial', 'sans-serif'],
+      },
       colors: {
         primary: {
           DEFAULT: '#b42828',
