a7da847058d026e6f090e80ba16e5d4e35b3f7e7
suico <ryoorer@gmail.com>
2024-05-20 01:53:55 +0900

    feat: flow

---
+++ src/app/_sections/flow.js
@@ -0,0 +1,8 @@
+export default function Flow() {
+  return (
+    <div>
+      <div className="h-1/5 w-full"></div>
+      <div className="w-full h-screen bg-green py-40">flow</div>
+    </div>
+  );
+}
