bcda2d6c40c318416dfa4933d83a50605374824b
suico <ryoorer@gmail.com>
2024-05-19 17:26:01 +0900

    feat: about

--- src/app/_sections/about.js
+++ src/app/_sections/about.js
@@ -1,7 +1,7 @@
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef, useEffect } from "react";
-import Image from "next/image";
+import gsap from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {useRef, useEffect} from 'react';
+import Image from 'next/image';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -29,58 +29,65 @@ export default function ScrollAnimatedComponent() {
   }, []);
 
   return (
-    <div ref={componentRef} className="w-full max-w-3xl mx-auto">
-      <p className="text-xl font-bold">
-        私たちは、デジタル・ローカル・コマースを
-        <br />
-        テーマに、地域の農作物の利活用・農業支援・
-        <br />
-        海洋環境保全に取り組みます。
-      </p>
-      <div className="flex justify-center w-full mb-20">
-        <div className="w-full text-xs font-bold leading-7 lg:pt-8">
-          <p className="mb-10">
-            個人経営店からグローバル展開する企業まで、
-            <br/>
-            様々な規模のプロジェクトで「CxO」として、
-            <br/>
-            プロトタイピングやリビルディングを主導し、
-            <br/>
-            社会にプラスの変化をもたらしたいと考える
-            <br/>
-            意思を具現化してきました。
-          </p>
-          <p>
-            支援会社は事業構造上、キャッシュが潤沢かつ
-            <br/>
-            予算があるプロジェクト以外は支援できません。
-            <br/>
-            他方、私たちは「予算がない」プロジェクトを
-            <br/>
-            中心に、支援会社と事業会社の両方を経験した
-            <br/>
-            スタッフが丁寧に対応します。
-          </p>
+    <div ref={componentRef}>
+      <div className="w-full max-w-3xl mx-auto">
+        <p className="text-xl font-bold">
+          私たちは、デジタル・ローカル・コマースを
+          <br />
+          テーマに、地域の農作物の利活用・農業支援・
+          <br />
+          海洋環境保全に取り組みます。
+        </p>
+        <div className="flex justify-center w-full mb-20">
+          <div className="w-full text-xs font-bold leading-7 lg:pt-8">
+            <p className="mb-10">
+              個人経営店からグローバル展開する企業まで、
+              <br />
+              様々な規模のプロジェクトで「CxO」として、
+              <br />
+              プロトタイピングやリビルディングを主導し、
+              <br />
+              社会にプラスの変化をもたらしたいと考える
+              <br />
+              意思を具現化してきました。
+            </p>
+            <p>
+              支援会社は事業構造上、キャッシュが潤沢かつ
+              <br />
+              予算があるプロジェクト以外は支援できません。
+              <br />
+              他方、私たちは「予算がない」プロジェクトを
+              <br />
+              中心に、支援会社と事業会社の両方を経験した
+              <br />
+              スタッフが丁寧に対応します。
+            </p>
+          </div>
+          <div className="flex justify-center w-full">
+            <Image
+              src="/about/1.png"
+              alt="TODO"
+              className="w-full lg:pl-8"
+              width="300"
+              height="300"
+            />
+          </div>
         </div>
-        <div className="flex justify-center w-full">
+        <div className="w-full">
           <Image
-            src='/about/1.png'
-            alt='TODO'
-            className='w-full lg:pl-8'
-            width='300'
-            height='300'
+            src="/about/2.png"
+            alt="TODO"
+            className="w-full pl-12"
+            width={300}
+            height={300}
           />
         </div>
       </div>
-      <div className="w-full">
-        <Image
-          src='/about/2.png'
-          alt='TODO'
-          className="w-full pl-12"
-          width={300}
-          height={300}
-        />
-      </div>
+      {/* パララックス */}
+      {/* <div>
+        <Image src="/parallax/1.png" alt="風景" width={300} height={300} />
+        <Image src="/parallax/2.png" alt="雲" width={300} height={300} />
+      </div> */}
     </div>
   );
 }
