3ff8b884e1183d5385a7c41c97c9fa68e9bebca4
Toshihiro Kojima <toshihiro.l.kojima@gmail.com>
2024-05-22 14:32:46 +0900

    feat: flow basement sp

--- src/app/_sections/flow.js
+++ src/app/_sections/flow.js
@@ -1,10 +1,20 @@
 'use client';
-import {useEffect, useState} from 'react';
+import {useRef, useEffect, useState} from 'react';
+import gsap from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import Image from 'next/image';
 import Button from './../_components/button';
 import {notoSansJP} from '../_utils/font';
 
 export default function Flow() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const containerRef = useRef(null);
+  const page1Ref = useRef(null);
+  const page2Ref = useRef(null);
+  const page3Ref = useRef(null);
+  const didEffect = useRef(false);
+
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
@@ -13,15 +23,52 @@ export default function Flow() {
         setCurrent(0);
         return;
       }
-
       setCurrent(prev => prev + 1);
     }, 512);
-
     return () => clearTimeout(id);
   }, [current]);
 
+  useEffect(() => {
+    if (didEffect.current) return;
+    didEffect.current = true;
+
+    const containerElement = containerRef.current;
+    const page1Element = page1Ref.current;
+    const page2Element = page2Ref.current;
+    const page3Element = page3Ref.current;
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: containerElement,
+          start: 'center center',
+          pin: true,
+          scrub: true,
+        },
+      })
+      .to(page1Element, {
+        opacity: 0,
+        y: 48,
+      })
+      .from(page2Element, {
+        opacity: 0,
+        y: 48,
+      })
+      .to(page2Element, {
+        opacity: 0,
+        y: 48,
+      })
+      .from(page3Element, {
+        opacity: 0,
+        y: 48,
+      });
+  }, []);
+
   return (
-    <div className="relative h-screen w-full bg-green overflow-hidden">
+    <div
+      ref={containerRef}
+      className="relative h-screen w-full bg-green overflow-hidden"
+    >
       <div className="absolute inset-0 z-0 flex justify-center items-center scale-125">
         <Image
           src={`/flow/bg/sp/${current + 1}.svg`}
@@ -32,8 +79,10 @@ export default function Flow() {
         />
       </div>
 
-      <div className="relative h-full z-10 flex justify-center items-center">
-        {/* <div className={`max-w-sm px-6`}> */}
+      <div
+        ref={page1Ref}
+        className="absolute inset-0 z-10 flex justify-center items-center"
+      >
         <div className={`max-w-sm px-6 ${notoSansJP.className}`}>
           <Image src="/cat/lead.png" alt="" width={800} height={567} />
 
@@ -43,10 +92,8 @@ export default function Flow() {
             INITIATIVE
           </p>
 
-          <p className="text-xs font-bold text-center leading-relaxed">
-            日々ご連絡いただく様々な悩みごとに対して、
-            <br />
-            オーダーメイドで施策を企画検討しています。
+          <p className="text-xs font-bold leading-relaxed text-justify">
+            日々ご連絡いただく様々な悩みごとに対して、オーダーメイドで施策を企画検討しています。
           </p>
 
           <div className="mt-7">
@@ -65,6 +112,80 @@ export default function Flow() {
           <div className="text-right mt-5">
             <Button />
           </div>
+        </div>
+      </div>
+
+      <div
+        ref={page2Ref}
+        className="absolute inset-0 z-10 flex justify-center items-center"
+      >
+        <div className={`max-w-sm px-6 ${notoSansJP.className}`}>
+          <Image src="/cat/walk.png" alt="" width={800} height={567} />
+
+          <h2 className="text-xl font-bold text-center mb-1">妖怪図鑑</h2>
+
+          <p className="text-xs font-mudial font-semibold tracking-wider text-center mb-5">
+            CASE
+          </p>
+
+          <p className="text-xs font-bold leading-relaxed text-justify">
+            私たちは一次情報を重んじ、日本中の空をとび、生産者・製造者の方と直接対話を重ねています。
+            <br />
+            <br />
+            そこでよく話題に出る事業展開上の困りごとを「妖怪」に見立てご紹介します。ここで登場する妖怪たちに心当たりがある方はお気軽にご相談ください。
+          </p>
+
+          <div className="flex justify-between mt-9">
+            <Image
+              src="/flow/materials/book.png"
+              alt=""
+              className="ml-2"
+              width={145}
+              height={103}
+            />
+
+            <Button />
+          </div>
+        </div>
+      </div>
+
+      <div
+        ref={page3Ref}
+        className="absolute inset-0 z-10 flex justify-center items-center"
+      >
+        <div className={`max-w-sm px-6 ${notoSansJP.className}`}>
+          <h2 className="text-xl font-bold text-center mb-1">
+            私たちのポリシー
+          </h2>
+
+          <p className="text-xs font-mudial font-semibold tracking-wider text-center mb-5">
+            OUR POLICY
+          </p>
+
+          <p className="text-xs font-bold leading-relaxed text-justify">
+            日本には、たくさんの「地域」が存在します。
+            どの地域もそれぞれ特長や魅力がありますが
+            事業視点では、地理的なハンディキャップを
+            抱える地域が多いのではないでしょうか。
+            また、生産者の高齢化・生活者の少子高齢化・
+            若者の人口流出などで、ハンディキャップの
+            固定化が進行しているのではないでしょうか。
+            <br />
+            <br />
+            私たちは、ハンディキャップの固定化により、
+            地域においてその克服に向けた新しい挑戦が
+            起きづらくなっていることを懸念しています。
+            <br />
+            <br />
+            もっと言えば、ハンディキャップの固定化は
+            日本全体の活力低下に直結すると考えており、
+            私たちは状況打破に向け、地域に眠る素材・
+            生産者・メーカーを発掘して支援します。
+            そこで生まれた、地域の「スター事業者」が
+            起点となり、「地域ブランド」づくりなどの
+            地理的なハンディキャップを克服するための
+            機運を作ることが、私たちの使命です。
+          </p>
         </div>
       </div>
     </div>
