6fd08c92138baaa0db4a256bde9b8d5404fb36d0
suico <ryoorer@gmail.com>
2024-05-26 12:44:21 +0900

    feat: isNumber

--- src/app/_sections/inNumbers.js
+++ src/app/_sections/inNumbers.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function InNumbers() {
   const items = [{image: ''}];
   return (
-    <div className="w-full pt-40">
+    <div className="relative w-full pt-40">
       <div className="flex flex-col lg:flex-row max-w-3xl mx-auto lg:max-w-7xl px-6 mb-8">
         <div className="flex-fix lg:mr-20 mb-4">
           <h2 className="text-4xl font-bold mb-4">
@@ -27,6 +27,25 @@ export default function InNumbers() {
               </div>
             );
           })}
+        </div>
+      </div>
+      {/* 雲 */}
+      <div className="absolute inset-0 -z-20 w-full h-full">
+        <div className="relative w-full h-full">
+          <Image
+            src="/cloud/1.png"
+            alt="雲"
+            className="cloud-animation-5 absolute top-4 right-2 lg:left-52 w-60"
+            width={100}
+            height={100}
+          />
+          <Image
+            src="/cloud/4.png"
+            alt="雲"
+            className="cloud-animation-1 absolute -bottom-12 right-0 lg:right-12 w-40"
+            width={100}
+            height={100}
+          />
         </div>
       </div>
     </div>
