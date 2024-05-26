e74abf779a4af3090d093220e5047361dc1f4551
suico <ryoorer@gmail.com>
2024-05-27 03:52:32 +0900

    update: news

--- src/app/_sections/news.js
+++ src/app/_sections/news.js
@@ -1,3 +1,6 @@
+import SectionTitle from '../_components/sectionTitle';
+import {notoSansJP} from '../_utils/font';
+
 export default function News() {
   const newsItems = [
     {
@@ -32,22 +35,23 @@ export default function News() {
       <div className="absolute inset-0 -z-10">
         <div className="bg-white h-full w-11/12 rounded-br-3xl"></div>
       </div>
-      <div className="flex flex-col lg:flex-row items-center max-w-3xl mx-auto px-6 lg:max-w-7xl">
+      <div className="flex flex-col lg:flex-row items-center max-w-md mx-auto px-6 lg:max-w-7xl">
         {/* タイトル */}
         <div className="w-full lg:w-1/5 flex-grow flex-shrink mb-8">
-          <h2 className="text-4xl font-bold mb-2 lg:mb-4">お知らせ</h2>
-          <p className="text-xl font-bold">NEWS</p>
+          <SectionTitle ja="お知らせ" en="NEWS" />
         </div>
         {/* アイテム */}
-        <div className="mr-10 lg:mr-20">
+        <div className="mr-10 lg:mr-32">
           {newsItems.map((item, i) => {
             return (
               <div
                 key={`news-${i}`}
                 className="flex flex-col lg:flex-row justify-center px-4 py-6 border-b border-primary lg:first:border-t border-dotted"
               >
-                <div className="mr-10 mb-4 lg:mb-0">{item.date}</div>
-                <div>{item.text}</div>
+                <div className="font-mundial mr-10 mb-4 lg:mb-0">
+                  {item.date}
+                </div>
+                <div className={`${notoSansJP.className}`}>{item.text}</div>
               </div>
             );
           })}
